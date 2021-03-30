096a0eb625f0a439b6efbda0e8f6b1ba25658939
Jamshid Qudratov <jamshidqudratov2324@gmail.com>
2021-03-30 12:10:02 +0500

    Initial commit

---
+++ src/Person.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+const Person = () => {
+    const [cats, setCats] = useState ([
+        {name: "Tom", color: "Red"},
+        {name: "Барсук", color: "Yellow"},
+        {name: "Garfield", color: "Orange"},
+    ])
+    
+
+    return (
+        <div>
+            <h3>Pets:</h3>
+            { cats.map(cat => (
+                <h2>Name: {cat.name}, Color: {cat.color}</h2>
+            ))}
+        </div>
+    )
+}
+
+export default Person;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Person from './Person'
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <Person/>,
+  document.querySelector('#root')
+);
+
+reportWebVitals();
